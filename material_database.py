ec025e03bece58c5e939264b0bf324d795c8e469
urquiagonzalo <urquiagonzalo@gmail.com>
2025-09-04 16:04:20 -0300

    Update material_database.py

--- material_database.py
+++ material_database.py
@@ -18,7 +18,8 @@ class MatDatabase(object):
 		
 		#%% Read in the dispersion data of each material
 		for i in range(self.num_materials):
-			file_name = './material_database/mat_' + self.material_key[i] + '.xlsx'
+			#file_name = './material_database/mat_' + self.material_key[i] + '.xlsx'
+			file_name = './porosidad_database/mat_' + self.material_key[i] + '.xlsx'
 			
 			try: 
 				A = np.array(pd.read_excel(file_name))

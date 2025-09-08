4f488564c2f2e5ad3007c928030c5848dba21850
urquiagonzalo <urquiagonzalo@gmail.com>
2025-09-08 15:14:15 -0300

    Update material_database.py

--- material_database.py
+++ material_database.py
@@ -11,8 +11,8 @@ class MatDatabase(object):
 		super(MatDatabase, self).__init__()
 		self.material_key = material_key
 		self.num_materials = len(material_key)
-		self.database_folder = database_folder
-		self.mat_database = self.build_database() #GU8/9: agregado para considerar distintas carpetas desde el código principal. 
+		self.database_folder = database_folder         #GU8/9: agregado para considerar distintas carpetas desde el código principal. 
+		self.mat_database = self.build_database() 
 
 	def build_database(self):
 		mat_database = {}

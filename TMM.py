c74f3e51ad87fcfe3da77587c3858bd200643046
urquiagonzalo <urquiagonzalo@gmail.com>
2025-09-13 22:22:56 -0300

    Update TMM.py

--- TMM.py
+++ TMM.py
@@ -186,6 +186,32 @@ def TMM_solver(self, thicknesses, refractive_indices, n_bot, n_top, k, theta, po
     Reflection = torch.pow(complex_abs(S_stack[2]), 2) / torch.pow(complex_abs(S_stack[3]), 2)
     
     #GU5/9: agregué para considerar transmisión
+
+    cos_theta_bot = torch.sqrt(1 - (ky / (k * n_bot + 0j))**2)
+    cos_theta_top = torch.sqrt(1 - (ky / (k * n_top + 0j))**2)
+    
+    # Calcular la parte real del cociente de impedancia, que representa flujo de potencia
+    if pol == 'TM':
+        numerator = torch.real(n_top * cos_theta_top)
+        denominator = torch.real(n_bot * cos_theta_bot)
+    elif pol == 'TE':
+        numerator = torch.real(n_bot * cos_theta_bot)
+        denominator = torch.real(n_top * cos_theta_top)
+    else:
+        numerator_TM = torch.real(n_top * cos_theta_top)
+        denominator_TM = torch.real(n_bot * cos_theta_bot)
+        numerator_TE = torch.real(n_bot * cos_theta_bot)
+        denominator_TE = torch.real(n_top * cos_theta_top)
+        numerator = torch.cat([numerator_TM, numerator_TE], dim=-1)
+        denominator = torch.cat([denominator_TM, denominator_TE], dim=-1)
+    
+    impedance_ratio = numerator / (denominator + 1e-12)  # para evitar división por 0
+    
+    # Transmitancia física
+    T22_abs2 = torch.pow(complex_abs(S_stack[3]), 2)
+    Transmission = impedance_ratio / (T22_abs2 + 1e-12)
+
+    """
     # Calcular cosenos de los ángulos en las interfaces
     cos_theta_bot = torch.sqrt(1 - (ky / (k * n_bot))**2)
     cos_theta_top = torch.sqrt(1 - (ky / (k * n_top))**2)
@@ -203,6 +229,7 @@ def TMM_solver(self, thicknesses, refractive_indices, n_bot, n_top, k, theta, po
     # Transmitancia
     T22_abs2 = torch.pow(complex_abs(S_stack[3]), 2)
     Transmission = (1.0 / T22_abs2) * torch.real(impedance_ratio)
+    """
 
     if self.spectra:        
         return Reflection
